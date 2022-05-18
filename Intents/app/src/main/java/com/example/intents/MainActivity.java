16cdade4e41a6eab2175c1c9f581f4391f85c27e
Nayab-Tahir <nayabtahir321@gmail.com>
2022-05-19 00:21:50 +0500

    Intents with Parameters Passing

--- Intents/app/src/main/java/com/example/intents/MainActivity.java
+++ Intents/app/src/main/java/com/example/intents/MainActivity.java
@@ -6,20 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn;
+    TextView msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btn = findViewById(R.id.button);
+        msg = findViewById(R.id.Msg);
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+
+                intent.putExtra("Message", msg.getText().toString());
                 startActivity(intent);
             }
         });
--- Intents/app/src/main/java/com/example/intents/MainActivity2.java
+++ Intents/app/src/main/java/com/example/intents/MainActivity2.java
@@ -2,13 +2,20 @@ package com.example.intents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    TextView output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        output = findViewById(R.id.outputMsg);
+        Intent intent = getIntent();
+        output.setText(intent.getStringExtra("Message"));
     }
 }
