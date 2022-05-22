2f3b47e4ef544673375bf571acad680b5d00d4b5
Nayab-Tahir <nayabtahir321@gmail.com>
2022-05-22 15:25:19 +0500

    LearningApp MainPage and ABC layout only

---
+++ LearningApp/app/src/main/java/com/example/learningapp/LearnHomeActivity.java
@@ -0,0 +1,23 @@
+package com.example.learningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LearnHomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnA, btnB, btnC, btnD, btnE, btnF, btnG, btnH, btnI, btnJ, btnK, btnL, btnM, btnN, btnO, btnP, btnQ,
+            btnR, btnS, btnT, btnU, btnV, btnW, btnX, btnY, btnZ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_home);
+    }
+
+    @Override
+    public void onClick(View view) {
+        
+    }
+}
---
+++ LearningApp/app/src/main/java/com/example/learningapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.learningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button repoBtn, learnBtn, testBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        repoBtn = findViewById(R.id.repoBtn);
+        learnBtn = findViewById(R.id.learnBtn);
+        testBtn = findViewById(R.id.testBtn);
+
+        repoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://github.com/Nayab-Tahir/MobileComputing");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        learnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LearnHomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
